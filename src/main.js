85616f231b6549528ff1a73a274f0e3bf916dd76
MohanSai1997 <tmohansk@gmail.com>
2020-11-30 21:42:14 +0530

    vue-analytics package is added

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./Routers"
 import vuetify from './plugins/vuetify';
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'G-7CL6863RH3',
+  router
+})
 
 new Vue({
   vuetify,
